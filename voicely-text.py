c2d59a2f2361cd798d003a8eeb683618377e6ccd
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 11:05:18 -0700

    removed guild sync

--- voicely-text.py
+++ voicely-text.py
@@ -472,23 +472,14 @@ async def leave(ctx: commands.Context):
 
 # region Manual sync command to sync slash commands globally or to a specific guild
 @bot.hybrid_command()
-async def sync(ctx: commands.Context, guild: return_same_or_none):
-    """Sync slash commands either globally or for a specific guild."""
-    if guild:
-        synced_commands = await bot.tree.sync(guild=guild)
-        command_list = ""
-        for command in synced_commands:
-            command_list += f"\n- {command.name}"
-            
-        await ctx.send(f"Commands synced to the guild: {guild.name}{command_list}", ephemeral=True)
-        
-    else:
-        synced_commands = await bot.tree.sync()
-        command_list = ""
-        for command in synced_commands:
-            command_list += f"\n- {command.name}"
+async def sync(ctx: commands.Context):
+    """Sync slash commands globally."""
+    synced_commands = await bot.tree.sync()
+    command_list = ""
+    for command in synced_commands:
+        command_list += f"\n- {command.name}"
 
-        await ctx.send(f"Commands synced globally:{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
+    await ctx.send(f"Commands synced globally:{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
 
 # endregion
 
