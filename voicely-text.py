5a79cbaa335c91facc80d21fef33d554cf2f2608
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 11:31:25 -0700

    debugging

--- voicely-text.py
+++ voicely-text.py
@@ -467,20 +467,23 @@ async def leave(ctx: commands.Context):
 
 # region Manual sync command to sync slash commands globally or to a specific guild
 @bot.hybrid_command()
-async def sync(ctx: commands.Context, guild = None):
+@app_commands.describe(guild="The server ID of the server you want to sync commands to.")
+async def sync(ctx: commands.Context, guild: discord.Guild = None):
     """Sync slash commands either globally or for a specific guild."""
+
+    print("sync triggered")
 
     if guild:
         synced_commands = await bot.tree.sync(guild=guild)
         command_list = ""
         for command in synced_commands:
-            command_list += f"\n- {command.name}"
+            command_list += f"\n\t- {command.name}"
         await ctx.send(f"Commands synced to the guild: {guild.name}{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
     else:
         synced_commands = await bot.tree.sync()
         command_list = ""
         for command in synced_commands:
-            command_list += f"\n- {command.name}"
+            command_list += f"\n\t- {command.name}"
         await ctx.send(f"Commands synced globally:{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
 
 # endregion
