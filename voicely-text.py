6211070171f7f98299268325fd4386322ea43c84
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 11:22:19 -0700

    Removed type for sync

--- voicely-text.py
+++ voicely-text.py
@@ -467,7 +467,7 @@ async def leave(ctx: commands.Context):
 
 # region Manual sync command to sync slash commands globally or to a specific guild
 @bot.hybrid_command()
-async def sync(ctx: commands.Context, guild: discord.Guild = None):
+async def sync(ctx: commands.Context, guild = None):
     """Sync slash commands either globally or for a specific guild."""
 
     if guild:
