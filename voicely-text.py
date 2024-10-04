d97b6ffb003cf6e9c089cc2270115962ab79146d
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 06:33:38 -0700

    Undid changes

--- voicely-text.py
+++ voicely-text.py
@@ -165,7 +165,7 @@ async def check_empty_channel():
         await interaction.response.send_message(content=f"Your choice is {self.values[0]}! ", ephemeral=True) """
 
 class LanguagesView(discord.ui.View):
-    options = []
+    # options = []
     
     langs = lang.tts_langs()
     keys = list(langs.keys())
@@ -175,14 +175,14 @@ class LanguagesView(discord.ui.View):
     # print(f"select_count = {select_count}")
     
     for x in range(select_count):
-        options.append([])
+        options = []
         # print(f"x = {x}")
 
         new_keys = keys[(x * 25):min((x * 25) + 25, len(keys))]
 
         for y in range(len(new_keys)):
             key = keys[y]
-            options[x].append(discord.SelectOption(label=langs[key], value=key, description=key))
+            options.append(discord.SelectOption(label=langs[key], value=key, description=key))
 
         # options.append(options)
         # iterated += 1
