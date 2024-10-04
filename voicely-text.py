2a9025fe47757ddc5e148ffa4ba6e055e8c202c0
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 12:30:20 -0700

    reformatted sync response again

--- voicely-text.py
+++ voicely-text.py
@@ -509,7 +509,7 @@ async def sync(ctx: commands.Context, guild: discord.Guild = None):
         synced_commands = await bot.tree.sync(guild=guild)
         command_list = ""
         for command in synced_commands:
-            command_list += f"\n  - `/{command.name}`"
+            command_list += f"\n- `/{command.name}`"
         await ctx.send(f"Commands synced to the guild: {guild.name}{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
     else:
         try:
@@ -525,7 +525,7 @@ async def sync(ctx: commands.Context, guild: discord.Guild = None):
         print("synced commands globally")
         command_list = ""
         for command in synced_commands:
-            command_list += f"\n  - `/{command.name}`"
+            command_list += f"\n- `/{command.name}`"
         await ctx.send(f"Commands synced globally:{command_list}\nPlease note it may take up to an hour to propagate globally.", ephemeral=True)
 
 # endregion
