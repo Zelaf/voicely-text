5fc2be0a37817167e59ff68b1b266c7ffb9055ed
Erallie <31375589+Erallie@users.noreply.github.com>
2024-10-04 08:10:57 -0700

    Adjusting formatting of settimout response

--- voicely-text.py
+++ voicely-text.py
@@ -287,7 +287,7 @@ def return_seconds(argument: int | str):
 async def settimeout(ctx: commands.Context, seconds: return_seconds):
     """Set the inactivity timeout duration."""
 
-    error_message = f"Please enter a valid timeout duration in **seconds**. Must be a positive whole number.\n\nAlternatively, type `reset` or `default` to reset the timeout to the default value *({bot.default_timeout} seconds)*."
+    error_message = f"Please enter a **positive whole number** to set the timeout duration in **seconds**.\n\nAlternatively, type `reset` or `default` to reset the timeout to the default value *({bot.default_timeout} seconds)*."
 
     if seconds == "reset" or seconds == "default" or seconds == bot.default_timeout:
         del bot.voice_channel_timeouts[ctx.guild.id]
