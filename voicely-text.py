3308e78b33834bf0aef6c231fbf469856ea0ea70
Erallie <31375589+Erallie@users.noreply.github.com>
2024-11-14 09:22:53 -0700

    Reapply "Updated sync command to only work for me"
    This reverts commit f465140b8a9ea9bf28249a6f4ddd9c7edb6eb9df.

--- voicely-text.py
+++ voicely-text.py
@@ -1577,12 +1577,13 @@ async def skip(ctx: commands.Context, count: return_int = 1):
 
 # region Sync
 @admin.command()
-@commands.is_owner()
 @app_commands.describe(guild="The server ID of the server you want to sync commands to.")
 async def sync(ctx: commands.Context, guild: discord.Guild = None):
     """Sync slash commands either globally or for a specific guild."""
 
-    # print("sync triggered")
+    if ctx.author.id != 339841608134557696:
+        ctx.send("This command can only be used by the owner of the bot.", reference=ctx.message, ephemeral=True)
+        return
 
     if guild:
         synced_commands = await bot.tree.sync(guild=guild)
